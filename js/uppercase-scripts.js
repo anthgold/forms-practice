788e9e349794cea7b92f5e40e48457488679006a
Anthony Gold and Kevin Deganos <anthony@>
2016-03-23 10:42:49 -0700

    condenses the convert to uppercase function into an argument within the preceding function

--- js/uppercase-scripts.js
+++ js/uppercase-scripts.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
 
     var nameInput = $("input#name").val();
 
-    var toUpperCaseName = nameInput.toUpperCase();
-
-    $(".name").text(toUpperCaseName);
-
+    // All three methods below work:
+      // var toUpperCaseName = nameInput.toUpperCase();
+      // $(".name").text(toUpperCaseName);
+      // nameInput = nameInput.toUpperCase();
+      // $(".name").text(nameInput);
+      $(".name").text(nameInput.toUpperCase());
+      // This one also should not change the value of name
+      // alert(nameInput); echoes the variable to show
+      
     $("#letter").show();
 
     event.preventDefault();
